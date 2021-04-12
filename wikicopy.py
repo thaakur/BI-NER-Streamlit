f8e4fad5b3135ed93268c3b3c9643167b5b2db0d
thaakur <avirms07@gmail.com>
2021-04-12 16:29:12 +0530

    update

--- wikicopy.py
+++ wikicopy.py
@@ -10,13 +10,16 @@ nlp = en_core_web_md.load()
 
 
 def main():
+	"""A NLP NER app with Spacy-Streamlit"""
+
 	st.title("Board Infinity TASK")
 	
 	st.subheader("Named Entity Recognition")
-	raw_text = st.text_area("Your Text","Enter Any Name")
-	docx = nlp(wikipedia.summary(raw_text,sentences=5))
-	if st.button("Display About Topic"):
-		spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)
+	for i in range(20):
+		raw_text = st.text_area("Your Text","Enter Any Name")
+		docx = nlp(wikipedia.summary(raw_text,sentences=5))
+		if st.button("Display About Topic"):
+			spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)
 
 
 if __name__ == '__main__':
